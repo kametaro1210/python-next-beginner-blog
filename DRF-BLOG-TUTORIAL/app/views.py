86999d62532f954b044075950c67563d0e98d989
kametaro1210 <hamuneko1210@gmail.com>
2026-09-15 11:14:17 +0900

    Views追加

--- DRF-BLOG-TUTORIAL/app/views.py
+++ DRF-BLOG-TUTORIAL/app/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+"""
+サーバー起動軌道をして、動作確認済み
+http://127.0.0.1:8000/api/post/
+http://127.0.0.1:8000/api/post/1/
+"""
+
+# from django.shortcuts import render
+
+# Django REST Frameworkの汎用APIViewを読み込む
+from rest_framework import generics
+
+# APIで返すデータの形式を定義したSerializerを読み込む
+from .serializers import PostSerializer
+
+# データベースのPostモデルを読み込む
+from .models import Post
+
+
+# 投稿一覧を取得するためのビュー
+class PostView(generics.ListAPIView):
+    # データベースからすべての投稿を取得する
+    queryset = Post.objects.all()
+
+    # 取得した投稿をJSON形式に変換するSerializerを指定
+    serializer_class = PostSerializer
+
+
+# 投稿の詳細を取得するためのビュー
+class PostDetailView(generics.RetrieveAPIView):
+    # データベースからPostのデータを取得する
+    queryset = Post.objects.all()
+
+    # 取得した投稿をJSON形式に変換するSerializerを指定
+    serializer_class = PostSerializer
