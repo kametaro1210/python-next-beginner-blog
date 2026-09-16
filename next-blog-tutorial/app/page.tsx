b9c2cc9dcfb9228845beb294eafe9a858da405c9
kametaro1210 <hamuneko1210@gmail.com>
2026-09-16 18:54:28 +0900

    topページ修正(画像表示)

--- next-blog-tutorial/app/page.tsx
+++ next-blog-tutorial/app/page.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <h1 className="flex h-screen items-center justify-center text-9xl text-indigo-500">
-        TailwindCSS
-    </h1>
+    <div>
+      <div className="mb-10 p-3 border rounded">
+        <Image
+          className="object-cover rounded"
+          src="/top.png"
+          alt="top"
+          width={1280}
+          height={500}
+        />
+      </div>
+      
+      <div className="flex justify-center flex-col items-center mb-10">
+        <div className="text-lg mb-3">BLOG POSTS</div>
+        <div className="border w-14"></div>
+      </div>
+    </div>
   );
 }
